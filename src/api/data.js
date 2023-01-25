72b54c5e81509bfc4d215fccddc507095658cbc0
Mauricio Oleaga <maoa.develoment@gmail.com>
2023-01-24 22:18:24 -0400

    Feat: create new file js and adding images to DB
    It was create a new file named data.js in a new folder name api, also was add images to the folder images-data for the DB

---
+++ src/api/data.js
@@ -0,0 +1,112 @@
+export const BEER_DATA = [
+	{
+		id: beer_01,
+		name: "Leffe (Blonde)",
+		country: "Belgium",
+		brewery: "Abbaye de Leffe S.A",
+		type: "Belgian Blonde Ale",
+		abv: 6.6,
+		img: "leffe-beer_clean.jpeg",
+		notes:
+			"Leffe Blond is an authentic blond abbey beer with a slight hint of bitterness to it. It is delicate but characteristic, Leffe Blonde is the tasting beer par excellence: it gives an extra touch of flavor to simple dishes and lightly accompanies your fried foods.",
+	},
+	{
+		id: beer_02,
+		name: "La Chouffe (Blonde)",
+		country: "Belgium",
+		brewery: "Brasserie d'Achouffe",
+		type: "Belgian Pale strong Ale",
+		abv: 8,
+		img: "laChouffe-beer.jpeg",
+		notes:
+			"La Chouffe, with its slight hoppy taste, combining notes of fresh coriander and fruity tones, is the drink which gives them their zest for life.",
+	},
+	{
+		id: beer_03,
+		name: "Kunstmanm Honig Ale",
+		country: "Chile",
+		brewery: "Compania Cervecera Kunstmann, S.A",
+		type: "Herb and Spice Beer",
+		abv: 4.8,
+		img: "kunsmanmMiel-beer.jpeg",
+		notes:
+			"The sweetness of the honey is appreciated subtly integrating itself between the toasted tones of the malt, with a soft bitterness as a counterpoint. Light amber in color, with sweet and malty notes with a hint of spice. Generally very sweet.",
+	},
+	{
+		id: beer_04,
+		name: "Gulden Draak Smoked",
+		country: "Belgium",
+		brewery: "Brouwerij Van Steenberge N.V",
+		type: "Quadrupel (Quad)",
+		abv: 10.5,
+		img: "guldenDraak-beer.jpeg",
+		notes:
+			"The Gulden Draak Smoked , It has a deep dark brown color with a smoky malt aroma and is Smoky in flavor with sweet notes of caramel, chocolate and black berries with light-sweet and smoky finishing touches.",
+	},
+	{
+		id: beer_05,
+		name: "Modelo (Negra)",
+		country: "Mexico",
+		brewery: "Grupo Modelo S.A. de C.V",
+		type: "Munich Dunkel",
+		abv: 5.4,
+		img: "negraModelo-beer.jpeg",
+		notes:
+			"Negra Modelo,  is a Munich-type beer with 5.3º of alcohol that offers a balanced flavor and a delicate aroma of dark malt, caramel and hops.",
+	},
+	{
+		id: beer_06,
+		name: "Kross Stout",
+		country: "Chile",
+		brewery: "Kross Microbrew Beer (Southern Brewing Company)",
+		type: "American Stout",
+		abv: 5.4,
+		img: "krossStout-beer.jpeg",
+		notes:
+			"Kross stout,  is a traditional style of beer of Irish origin, characterized by its intense flavors of caramel, roasted coffee and dark chocolate. Its deep, distinctive dark ruby color and creamy head are the result of producing Pilsen, Caramel and Toast barley malts in small quantities.",
+	},
+	{
+		id: beer_07,
+		name: "Duvel",
+		country: "Belgium",
+		brewery: "Brouwerij Duvel Moortgat NV",
+		type: "Belgian Pale strong Ale",
+		abv: 8.5,
+		img: "duvel-beer.jpeg",
+		notes:
+			"Duvel is lively and tingling on the nose with a citrus element that even tends towards grapefruit thanks to the use of the highest quality hop varieties. This is also reflected in the taste, which is beautifully balanced with a hint of spiciness. Thanks to its high CO2 content, this beer has a wonderful roundness on the palate.",
+	},
+	{
+		id: beer_08,
+		name: "Orval Trappist Ale",
+		country: "Belgium",
+		brewery: "Brasserie d'Orval S.A",
+		type: "Belgian Pale Ale",
+		abv: 6.2,
+		img: "orval-beer.jpeg",
+		notes:
+			"Orval Trappist Ale, the ageing process adds a fruity note, which strikes a subtle balance between the beer’s full-bodied yet complex flavour and bitterness. The brewery has selected very aromatic and unique hop varieties, which hark back to the first brewmaster of Orval, who hailed from Bavaria. The beer’s aromas are very pronounced while maintaining the right level of bitterness thanks to the English method of dry hopping.",
+	},
+	{
+		id: beer_09,
+		name: "Chimay Bleue (Blue)",
+		country: "Belgium",
+		brewery: "Bières de Chimay",
+		type: "Belgian Dark strong Ale",
+		abv: 9,
+		img: "chimay-beer.jpeg",
+		notes:
+			"Chiamy Bleue, when pouring it presents a dark brown color with long-lasting ivory foam. Chimay Blue pumps out extremely complex aromas of whole wheat bread, molasses, dark fruits, and spices that lead to a rich palate with remarkable alcoholic warmth.",
+	},
+	{
+		id: beer_10,
+		name: "Benediktiner Weissbier",
+		country: "Germany",
+		brewery: "Klosterbrauerei Ettal",
+		type: "Hefeweizen",
+		abv: 5.4,
+		img: "benediktiner-beer.jpeg",
+		notes:
+			"Benediktiner Weissbier, This special type of brewing allows a particularly full and also very tasty wheat beer. The amber color comes through special malt, which also brings a caramel-like character and other taste facets in the beer.",
+	},
+];
